0429c8ee18e89a7e92a2f5c85ccbdcf084961421
Prerna <prerna09102k3@gmail.com>
2021-06-04 05:42:42 +0530

    Game file

---
+++ game.py
@@ -0,0 +1,29 @@
+#Project 2
+import random
+randNumber = random.randint(1,100)
+#print(randNumber)
+userGuess = None
+guesses = 0
+
+
+while(userGuess != randNumber):
+    userGuess = int(input("Enter your guess:\n"))
+    guesses += 1
+    if(userGuess==randNumber):
+        print("You guessed it Right!!")
+    else:
+        
+        if(userGuess>randNumber):
+            print("You guessed it wrong!! Enter a smaller number")
+        else:
+            print("You guessed it wrong!! Enter a larger number")
+            
+print(f"YOu Guessed the number in {guesses} guesses")
+
+
+with open("hiscore.txt","r") as f:
+    hiscore = int(f.read())
+if(guesses<hiscore):
+    print("You've just broken the high score ")
+    with open("hiscore.txt","w") as f:
+        f.write(str(guesses))
